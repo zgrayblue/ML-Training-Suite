9c47f8c8aacb92f9f6a545bd058c203e142493fc
Florian Wiesner <65916600+FloWsnr@users.noreply.github.com>
2025-09-16 13:33:39 +0200

    Modify get_dataset to include split parameter
    Added split parameter to get_dataset function for dataset splitting.

--- ml_suite/data/dataset.py
+++ ml_suite/data/dataset.py
@@ -1,5 +1,6 @@
 import torch
 
 
-def get_dataset(config: dict) -> torch.utils.data.Dataset:
+def get_dataset(config: dict, split: str="train") -> torch.utils.data.Dataset:
+    # Make sure to split your data somehow, either return two datasets here or use the split option
     return torch.utils.data.Dataset()  # Placeholder for actual dataset implementation
